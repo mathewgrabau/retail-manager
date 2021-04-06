fcf193a232eb5632be76538610cb2bccd116c5a4
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-04-05 21:18:01 -0500

    Configured WPF project, database project and init the Caliburn framework.

---
+++ RetailManager-WPF-UI/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using RetailManager_WPF_UI.ViewModels;
+using System.Windows;
+
+namespace RetailManager_WPF_UI
+{
+    /// <summary>
+    /// Setup the Caliburn.Micro framework.
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
