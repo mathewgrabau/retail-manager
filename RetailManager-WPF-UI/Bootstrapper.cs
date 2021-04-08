961808f88bbd2772d89f06bfe1260e64ed08e890
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-04-07 23:27:37 -0500

    Added the bootstrap class.

--- RetailManager-WPF-UI/Bootstrapper.cs
+++ RetailManager-WPF-UI/Bootstrapper.cs
@@ -1,5 +1,7 @@
 ﻿using Caliburn.Micro;
 using RetailManager_WPF_UI.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace RetailManager_WPF_UI
@@ -9,6 +11,8 @@ namespace RetailManager_WPF_UI
     /// </summary>
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
@@ -17,6 +21,22 @@ namespace RetailManager_WPF_UI
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            // Refer to the container for creating the objects to inject.
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
