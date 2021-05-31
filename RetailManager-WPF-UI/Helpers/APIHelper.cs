62dbef7e99483ce62d74b7025e8e44a48aba807c
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-05-31 07:50:37 -0500

    In progress (API)

---
+++ RetailManager-WPF-UI/Helpers/APIHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManager_WPF_UI.Helpers
+{
+    /// <summary>
+    /// Abstraction for views to use (requiring it)
+    /// </summary>
+    public class APIHelper
+    {
+        public HttpClient Client { get; private set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public void Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+            });
+        }
+    }
+}
